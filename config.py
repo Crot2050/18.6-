79c31d73b84d936e19a1071688cabe8582594cd1
Makova Nataliya <116758787+Crot2050@users.noreply.github.com>
2023-03-08 22:33:37 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = '6116941963:AAHpiLRaeEEmuQNQMsTPFIJOjlIuRxIDkkc'
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'йены': 'JPY',
+    'евро': 'EUR',
+    'рубли': 'RUB'
+}
